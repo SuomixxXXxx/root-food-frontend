985a9e719d86b5112f49e14ebd2e138994d3a1b3
tanyyaako <tanyyaako@icloud.com>
2024-09-15 16:15:28 +0300

    [feat]: registration window

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+
 export default function TextField() {
     return (
-        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs pl-4  border-2 border-[#475569] " />
     )
 }
--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+
 import TextField from "../components/TextField";
 export default function RegistrationPage(){
   return(
-        <div className=" flex flex-col place-content-center">
+    <div className="flex items-center h-screen justify-center ">
+      
+      <div className=" flex flex-col space-y-4 border-2 border-[#475569] items-center ">
+      <p>Регистрация</p>
           <TextField/>
           <TextField/>
           <TextField/>
         </div>
-      
-        
-    
+    </div>
   )
 
 }
