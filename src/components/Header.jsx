83be214ee2fe318af3b9ff359554f5dd8a9e9307
Suomi228 <bixep345@gmail.com>
2024-09-15 20:13:47 +0300

    [feat]: basic input

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,67 +1,58 @@
-import React from 'react'
-
+import React from "react";
+import {
+  Navbar,
+  Typography,
+  IconButton,
+  Button,
+  Input,
+} from "@material-tailwind/react";
+import { BellIcon, Cog6ToothIcon } from "@heroicons/react/24/solid";
 export default function Header() {
   return (
-    <div>
-      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-        <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <svg
-            className="fill-current h-8 w-8 mr-2"
-            width="54"
-            height="54"
-            viewBox="0 0 54 54"
-            xmlns="http://www.w3.org/2000/svg"
+    <Navbar
+      variant="gradient"
+      color="blue-gray"
+      className="max-w-full rounded-none from-blue-gray-900 to-blue-gray-800"
+    >
+      <div className="flex flex-row justify-between flex-wrap">
+        <Button>Hello</Button>
+
+        <div className="relative flex w-full max-w-[28rem]">
+          <Input
+            type="email"
+            label="Поиск"
+            // value={email}
+            // onChange={onChange}
+            className="pr-20"
+            containerProps={{
+              className: "min-w-0",
+            }}
+          />
+          <Button
+            size="sm"
+            // color={email ? "gray" : "blue-gray"}
+            // disabled={!email}
+            className="!absolute right-1 top-1 bottom-1 rounded"
           >
-            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-          </svg>
-          <span className="font-semibold text-xl tracking-tight">
-            Tailwind CSS
-          </span>
-        </div>
-        <div className="block lg:hidden">
-          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
             <svg
-              className="fill-current h-3 w-3"
-              viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="size-5"
             >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+              />
             </svg>
-          </button>
+          </Button>
         </div>
-        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-          <div className="text-sm lg:flex-grow">
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-            >
-              Docs
-            </a>
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-            >
-              Examples
-            </a>
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
-            >
-              Blog
-            </a>
-          </div>
-          <div>
-            <a
-              href="#"
-              className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-            >
-              Download
-            </a>
-          </div>
-        </div>
-      </nav>
-      
-    </div>
-  )
+
+        <Button>Hello</Button>
+      </div>
+    </Navbar>
+  );
 }
