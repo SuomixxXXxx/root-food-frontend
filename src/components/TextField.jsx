26b3c53c3ac8c966be2b1b2494ccfcec3ea6a8a5
tanyyaako <tanyyaako@icloud.com>
2024-09-16 21:43:00 +0300

    feat]: box with TextFields and button of registration

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,6 +1,11 @@
+import React from 'react';
 
-export default function TextField() {
+export const TextField = (props) => {
+    const {placeholder} = props;
     return (
-        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs pl-4  border-2 border-[#475569] " />
+        <div>
+            <input type="text" placeholder={placeholder} style={{ borderRadius: '10px', marginLeft:"2em",marginRight:"2em", width:"200px"}}
+                   className="input input-bordered w-full max-w-xs pl-4  border-2 border-[#475569] "/>
+        </div>
     )
-}
+};
--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,16 +1,25 @@
+import {TextField} from "../components/TextField.jsx";
 
-import TextField from "../components/TextField";
-export default function RegistrationPage(){
-  return(
-    <div className="flex items-center h-screen justify-center ">
-      
-      <div className=" flex flex-col space-y-4 border-2 border-[#475569] items-center ">
-      <p>Регистрация</p>
-          <TextField/>
-          <TextField/>
-          <TextField/>
+export default function RegistrationPage() {
+    return (
+        <div className="flex items-center h-screen justify-center font-semibold " style={{ borderRadius: '30px'}}>
+
+            <div className=" flex flex-col space-y-4  border-[#475569] items-center "style={{ borderRadius: '30px', boxShadow: "2px 2px 10px grey",border:"#475569"}}>
+                <p className={"m-2"} style={{fontSize: '25px'}}>Регистрация</p>
+                <TextField placeholder={"Имя"}/>
+                <TextField placeholder={"Фамилия"}/>
+                <TextField placeholder={"Почта"}/>
+                <TextField placeholder={"Пароль"}/>
+                <div>
+                    <button style={{color: 'white', background: "#14B8A6", fontSize: '15px', borderRadius: '10px', border: '1rem solid #14B8A6' }} className={"m-4"}>Зарегистрироваться</button>
+
+                </div>
+                <div className="flex flex-row " style={{marginBottom:"2em"}}>
+                    <p style={{color: 'grey',marginRight:"0.5em"}}>Есть аккаунт? </p>
+                    <button> Войти</button>
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 
 }
