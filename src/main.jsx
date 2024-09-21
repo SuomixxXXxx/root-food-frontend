4cbe42a923f6758b6e359ae9a1c3a1ec36a91876
tanyyaako <tanyyaako@icloud.com>
2024-09-21 22:08:52 +0300

    fix: dependencies fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,6 @@ import './index.css'
 import { ThemeProvider } from "@material-tailwind/react";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <App />
-</StrictMode>,
-)
     <ThemeProvider>
       <App />
     </ThemeProvider>
