84b955a6077724c55ae320be9da9d6fd66e13c96
dvrnc <darryvoron1404@gmail.com>
2024-09-21 11:35:13 +0300

    feat:basic card

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 import withMT from "@material-tailwind/react/utils/withMT";
- 
+
 export default withMT({
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 });
-
