ea3e1bf52ae19370c89583bf71f1d29fffd5777b
tanyyaako <tanyyaako@icloud.com>
2024-09-21 22:41:42 +0300

    feat: complete login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Header from "./components/Header";
 import "./App.css";
 import RegistrationPage from "./pages/RegistrationPage";
 import CartPage from "./pages/CartPage";
+import LoginPage from "./pages/LoginPage";
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +12,8 @@ function App() {
   return (
     <>
       <Header/>
-      <RegistrationPage/>
+      <LoginPage/>
+      {/* <RegistrationPage/> */}
       {/* <CartPage/> */}
     </>
 
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,30 +5,31 @@ import {
     CardFooter,
     Typography,
     Button,
+    Checkbox,
   } from "@material-tailwind/react";
   import aquariumLogo from "../assets/aquariumLogo.svg";
    
 
-export default function RegistrationPage() {
+export default function LoginPage() {
     return (
         <div className="h-screen bg-blue-gray-100 flex justify-center items-center ">
             <Card  className="flex items-center flex-col  w-80  ">
-            <div className="mt-4"><img src={aquariumLogo} alt="My aquariumLogo" /></div>
-                <CardBody className="flex flex-col pt-0">
-                    <TextField placeholder={"Имя"}/>
-                    <TextField placeholder={"Номер телефона"}/>
+                <div className="mt-4"><img src={aquariumLogo} alt="My aquariumLogo" /></div>
+                <CardBody className="flex flex-col pt-0 ">
                     <TextField placeholder={"Почта"}/>
                     <TextField placeholder={"Пароль"}/>
+                    <div className="-ml-2.5 ">
+                        <Checkbox color="blue" label="Запомнить меня" />
+                     </div>
                 </CardBody>
-
                 <CardFooter className="pt-0 w-80" >
                     <Button   fullWidth color="blue">
-                        Зарегистрироваться
+                        Войти
                     </Button>
                     <Typography variant="small" className="mt-6 flex justify-center">
-                        Есть аккаунт?
-                    <Typography as="a" href="#войти" variant="small" color="blue-gray" className="ml-1 font-bold">
-                        Войти
+                        Нет аккаунта?
+                    <Typography as="a" href="#зарегистрироваться" variant="small" color="blue-gray" className="ml-1 font-bold">
+                        Зарегистрироваться
                     </Typography>
                     </Typography>
                 </CardFooter>
