a1c6cbb772600ded4345c78d3c093e152e4ae786
Suomi228 <bixep345@gmail.com>
2024-09-29 14:37:03 +0300

    feat: add routes in header

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,29 @@ import ProductCard from "./components/ProductCard";
 import RegistrationPage from "./pages/RegistrationPage";
 import CartPage from "./pages/CartPage";
 import LoginPage from "./pages/LoginPage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage/>,
+    children: [
+      { path: "signup", element: <RegistrationPage/> },
+      { path: "cart", element: <CartPage/> },
+      { path: "login", element: <LoginPage/> },
+    ]
+  }
+])
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Header/>
-      {/* <ProductCard name ="Сосиска в тесте" weight = {10} price = {100} /> */}
-      {/* <LoginPage/> */}
-      <RegistrationPage/>
-      {/* <CartPage/> */}
+      <RouterProvider router={router}/>
     </>
 
   );
