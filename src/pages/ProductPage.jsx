b080803f094bf8bda00eb50638c6a8d745cadb0f
tanyyaako <tanyyaako@icloud.com>
2024-10-10 22:07:53 +0300

    feat:prodeucts done?

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -86,13 +86,13 @@ const products = [
 
 export default function ProductPage() {
   return (
-    <div className=" flex flex-col md:flex-row min-h-screen bg-blue-gray-100 pr-4 pl-4 md:pr-10 md:pl-10 pb-0">
-      <div className="basis-full md:basis-1/4 mt-10 md:mt-28 ml-0 md:ml-10">
+    <div className=" flex flex-col bg-blue-gray-100 pr-4 pl-4 md:flex-row min-h-screen md:pr-10 md:pl-10 pb-0">
+      <div className="hidden md:flex basis-1/4 mt-10 md:mt-28 ml-0 md:ml-10">
         <SidebarCategory />
       </div>
 
-      <div className="mt-10 md:mt-28 mr-0 md:mr-10 ">
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+      <div className="mt-20 md:mt-28 mr-0 md:mr-10 ">
+        <div className="grid grid-cols-2 place-items-center md:grid-cols-3 gap-4">
         {products.map((obj, index) => (
             <ProductCard key={index} name={obj.name} weight={obj.weight} price={obj.price} />
           ))}
