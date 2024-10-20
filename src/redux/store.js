032d0739f47d2ad9c032b689704ed5ea39768850
Suomi228 <bixep345@gmail.com>
2024-10-20 15:46:32 +0300

    feat: добавил persis redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,13 +3,29 @@ import { dishItemReducer } from "./slices/dishItem";
 import { authReducer } from "./slices/auth";
 import { categoriesReducer } from "./slices/categories";
 import { cartReducer } from "./slices/cart";
+import storage from "redux-persist/lib/storage"
+import {persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage: storage,
+  whitelist: ['cart']
+}
+
+const reducer = combineReducers({
+  dishItems: dishItemReducer,
+  auth: authReducer,
+  categories: categoriesReducer,
+  cart: cartReducer,
+
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 const store = configureStore({
-  reducer: {
-    dishItems: dishItemReducer,
-    auth: authReducer,
-    categories: categoriesReducer,
-    cart: cartReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
