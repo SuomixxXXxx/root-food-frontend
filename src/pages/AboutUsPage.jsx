2dd4b912cbb3d2a0a8dc507ef6cee70350010a39
Suomi228 <bixep345@gmail.com>
2025-03-20 22:28:04 +0300

    feat: base about us

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,5 +1,27 @@
 export default function AboutUsPage() {
   return (
-    <div className="h-screen bg-light-blue flex justify-center items-center"></div>
+    <div className="h-screen bg-light-blue flex">
+      <div className="flex flex-col p-6 mt-20">
+        <h1 className="text-3xl font-bold">О нас</h1>
+        <div className="flex md:flex-row md:flex-wrap">
+          <div className="flex-1">
+            <ul className="text-xl">
+              График работы:
+              <li>Пн-Пт: 9:00 - 18:30</li>
+              <li>Сб: 9:00 - 17:00</li>
+              <li>
+                Вс: <span className="text-dark-red">выходной</span>
+              </li>
+            </ul>
+          </div>
+          <div className="flex-1">
+            <p className="text-lg">
+              Наш адрес: Новосущёвская ул., 22, стр. 1, Москва, этаж 1
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
   );
 }
